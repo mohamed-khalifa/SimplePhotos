92f87f6930d973442e2c10268f8eead3f78c590d
MohamedKhalifa <mohamed.mohy.khalifa@gmail.com>
2018-09-16 22:30:24 +0200

    add navigation to photo pager

--- app/src/main/java/com/mohamedkhalifa/simplephotos/data/FlickrDataSource.kt
+++ app/src/main/java/com/mohamedkhalifa/simplephotos/data/FlickrDataSource.kt
@@ -17,8 +17,8 @@ class FlickrDataSource(photos: MutableLiveData<List<PhotoUIModel>>, error: Mutab
         val currentPage = photoContainerUIModel.currentPage
         if (numberOfPages == null || currentPage <= numberOfPages) {
             executeRequest(ApiConnection.getRetrofitCall().create(ApiRetrofit::class.java).getPhotos(page = currentPage), photos, error, photoContainerUIModel)
-        }else{
-         photos.value = photoContainerUIModel.photos
+        } else {
+            photos.value = photoContainerUIModel.photos
         }
 
     }
@@ -36,11 +36,9 @@ class FlickrDataSource(photos: MutableLiveData<List<PhotoUIModel>>, error: Mutab
                     val flickrPhotosResponse = response.body()?.flickrApiPhotos
                     flickrPhotosResponse.let { flickrApiPhotos ->
                         if (flickrApiPhotos?.page != null) {
-                            photoContainerUIModel.currentPage = flickrApiPhotos.page+1
+                            photoContainerUIModel.currentPage = flickrApiPhotos.page + 1
                         }
                         photoContainerUIModel.numberOfPages = flickrApiPhotos?.pages
-
-
                         val flickrUIList: List<PhotoUIModel>? = flickrApiPhotos?.flickrApiPhotosList?.map {
                             mapToPhotoUIModel(it?.urlM, it?.urlSq)
                         }
@@ -54,8 +52,6 @@ class FlickrDataSource(photos: MutableLiveData<List<PhotoUIModel>>, error: Mutab
                             photos.value = photoContainerUIModel.photos
                         }
                     }
-
-
                 } else {
                     error.value = "error"
                 }
@@ -66,9 +62,5 @@ class FlickrDataSource(photos: MutableLiveData<List<PhotoUIModel>>, error: Mutab
     override fun mapToPhotoUIModel(photoUrl: String?, thumbnailUrl: String?): PhotoUIModel {
         return PhotoUIModel(photoUrl, thumbnailUrl)
     }
-    //    https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=827e2a77740cde8f23382e337d6e47a3&gallery_id=72157662070816797&per_page=10&extras=url_o&page=1&format=json&nojsoncallback=1
-//    https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=827e2a77740cde8f23382e337d6e47a3&gallery_id=72157662070816797&per_page=30&extras=url_o&page=1&format=json&nojsoncallback=1&auth_token=72157700823874554-f9d8a8a2d4dcd904&api_sig=2c635cad9dc77a9288cc196b09d46af8
-//    https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=827e2a77740cde8f23382e337d6e47a3&gallery_id=72157662070816797&per_page=30&extras=url_o&page=1&format=json&nojsoncallback=1&auth_token=72157700823874554-f9d8a8a2d4dcd904&api_sig=a4a652149587cd29f43cbea44ecdf441
-
 
 }
--- app/src/main/java/com/mohamedkhalifa/simplephotos/view/PhotosActivity.kt
+++ app/src/main/java/com/mohamedkhalifa/simplephotos/view/PhotosActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.mohamedkhalifa.simplephotos.R
 
-class PhotosActivity : AppCompatActivity() {
+class PhotosActivity : AppCompatActivity(), PhotosGridFragment.OnPhotoGridItemSelected {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photos)
 
         val photosGridFragment = PhotosGridFragment()
-
         supportFragmentManager.beginTransaction()
-                .replace(R.id.contentFrameLayout, photosGridFragment)
+                .add(R.id.contentFrameLayout, photosGridFragment)
+                .commit()
+    }
+
+    override fun onPhotoGridItemSelected(selectedPhotoIndex: Int) {
+        val photosPagerFragment = PhotosPagerFragment.newInstance(selectedPhotoIndex)
+        supportFragmentManager.beginTransaction()
+                .add(R.id.contentFrameLayout, photosPagerFragment)
+                .addToBackStack(null)
                 .commit()
     }
 }
